00b110d3e5933ac1ef49df0a59dafdd60ea56390
UGUR BARAN DUNDAR <ubarandundar@gmail.com>
2023-02-02 15:28:37 +0300

    updated

--- app.js
+++ app.js
@@ -35,38 +35,106 @@ adres.addEventListener('input', function () {
     pAdres.innerText = this.value;
 });
 
-// Roksit and DNSSense Create Image Event
-const company = document.querySelector('#firma');
+// // Roksit and DNSSense Create Image Event (related to dropdown menu)
+// const company = document.querySelector('#firma');
+// const sigFigure = document.querySelector('#sig-figure');
+// company.addEventListener('change', function () {
+//     const sigImg = document.querySelector('.sig-image');
+//     if (sigImg) {
+//         sigImg.remove();
+//     } if (this.value === 'Roksit') {
+//         const newFigure = document.createElement('img');
+//         newFigure.classList.add('sig-image');
+//         newFigure.setAttribute('src', '/assets/RoksitLogoSlogan.png')
+//         sigFigure.append(newFigure);
+//     } else if (this.value === 'DNSSense') {
+//         const newFigure = document.createElement('img');
+//         newFigure.classList.add('sig-image');
+//         newFigure.setAttribute('src', '/assets/DNSSenseLogo.svg')
+//         sigFigure.append(newFigure);
+//     }
+// });
+
+// Roksit and DNSSense Create Image Event (for Roksit)
+const sigRoksitRadio = document.querySelector('#sig-roksit-radio');
 const sigFigure = document.querySelector('#sig-figure');
-company.addEventListener('change', function () {
-    if (this.value === 'Roksit') {
-        const newFigure = document.createElement('img');
-        newFigure.classList.add('sig-image');
-        newFigure.setAttribute('src', '/assets/RoksitLogoSlogan.png')
-        sigFigure.append(newFigure);
-    } else if (this.value === 'DNSSense') {
-        const newFigure = document.createElement('img');
-        newFigure.classList.add('sig-image');
-        newFigure.setAttribute('src', '/assets/DNSSenseLogo.svg')
-        sigFigure.append(newFigure);
-    }
+sigRoksitRadio.addEventListener('change', function () {
+    const sigImg = document.querySelector('.sig-image');
+    if (sigImg) {
+        sigImg.remove();
+    } const newFigure = document.createElement('img');
+    newFigure.classList.add('sig-image');
+    newFigure.setAttribute('src', '/assets/RoksitLogoSlogan.png')
+    sigFigure.append(newFigure);
 });
 
-// Roksit and DNSSense Add Website Adress Event
-const emptyRightSide = document.querySelector('.empty-rightside');
-company.addEventListener('change', function () {
-    if (this.value === 'Roksit') {
-        const webSite = document.createElement('div');
-        webSite.classList.add('sig-website');
-        webSite.innerText = 'www.roksit.com';
-        emptyRightSide.append(webSite);
-    } else if (this.value === 'DNSSense') {
-        const webSite = document.createElement('div');
-        webSite.classList.add('sig-website');
-        webSite.innerText = 'www.dnssense.com';
-        emptyRightSide.append(webSite);
-    }
+// Roksit and DNSSense Create Image Event (for DNSSense)
+const sigDNSSenseRadio = document.querySelector('#sig-dnssense-radio');
+sigDNSSenseRadio.addEventListener('change', function () {
+    const sigImg = document.querySelector('.sig-image');
+    if (sigImg) {
+        sigImg.remove();
+    } const newFigure = document.createElement('img');
+    newFigure.classList.add('sig-image');
+    newFigure.setAttribute('src', '/assets/DNSSenseLogo.svg')
+    sigFigure.append(newFigure);
 });
+
+
+// Roksit and DNSSense Add Website Adress Event (for Roksit)
+const emptyRightSide = document.querySelector('.empty-rightside');
+sigRoksitRadio.addEventListener('change', function () {
+    const sigWebsite = document.querySelector('.sig-website');
+    if (sigWebsite) {
+        sigWebsite.remove();
+    } const webSite = document.createElement('div');
+    webSite.classList.add('sig-website');
+    webSite.innerText = 'www.roksit.com';
+    emptyRightSide.append(webSite);
+});
+
+// Roksit and DNSSense Add Website Adress Event (for DNSSense)
+sigDNSSenseRadio.addEventListener('change', function () {
+    const sigWebsite = document.querySelector('.sig-website');
+    if (sigWebsite) {
+        sigWebsite.remove();
+    } const webSite = document.createElement('div');
+    webSite.classList.add('sig-website');
+    webSite.innerText = 'www.dnssense.com';
+    emptyRightSide.append(webSite);
+});
+
+// Radio Buttons Selection (can made in JS if requested)
+// const formCheckInput = document.querySelector('.form-check-input');
+// const radioButtons = document.querySelectorAll('input[name="company"]');
+// formCheckInput.addEventListener("click", () => {
+//     let selectedCompany;
+//     for (const radioButton of radioButtons) {
+//         if (radioButton.checked) {
+//             selectedCompany = radioButton.value;
+//             break;
+//         }
+//     }
+// });
+
+// // Roksit and DNSSense Add Website Adress Event (related to dropdown menu)
+// const emptyRightSide = document.querySelector('.empty-rightside');
+// company.addEventListener('change', function () {
+//     const sigWebsite = document.querySelector('.sig-website');
+//     if (sigWebsite) {
+//         sigWebsite.remove();
+//     } if (this.value === 'Roksit') {
+//         const webSite = document.createElement('div');
+//         webSite.classList.add('sig-website');
+//         webSite.innerText = 'www.roksit.com';
+//         emptyRightSide.append(webSite);
+//     } else if (this.value === 'DNSSense') {
+//         const webSite = document.createElement('div');
+//         webSite.classList.add('sig-website');
+//         webSite.innerText = 'www.dnssense.com';
+//         emptyRightSide.append(webSite);
+//     }
+// });
 
 // Download Event
 const downloadButton = document.querySelector('.download');
@@ -100,19 +168,19 @@ bannerDeleteButton.addEventListener('click', function () {
     myImg.remove();
 });
 
-// Logo Delete Event
-const logoDeleteButton = document.querySelector('#logo-delete-button');
-logoDeleteButton.addEventListener('click', function () {
-    const sigImg = document.querySelector('.sig-image');
-    sigImg.remove();
-});
+// // Logo Delete Event (no need anymore)
+// const logoDeleteButton = document.querySelector('#logo-delete-button');
+// logoDeleteButton.addEventListener('click', function () {
+//     const sigImg = document.querySelector('.sig-image');
+//     sigImg.remove();
+// });
 
-// Website Delete Event
-const websiteDeleteButton = document.querySelector('#website-delete-button');
-websiteDeleteButton.addEventListener('click', function () {
-    const sigWebsite = document.querySelector('.sig-website');
-    sigWebsite.remove();
-});
+// // Website Delete Event (no need anymore)
+// const websiteDeleteButton = document.querySelector('#website-delete-button');
+// websiteDeleteButton.addEventListener('click', function () {
+//     const sigWebsite = document.querySelector('.sig-website');
+//     sigWebsite.remove();
+// });
 
 // Banner Create Event
 const addBannerButton = document.querySelector('.add-banner-button');
